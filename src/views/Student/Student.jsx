f6a2c9590c202f9f93d1253660c7cbef52554b1c
Anuj <anujvermaatc1994@gmail.com>
2019-06-02 23:54:25 +0530

    deleted  action

--- src/views/Student/Student.jsx
+++ src/views/Student/Student.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import AuthenticatedPage from "../AuthenticatedStudent";
 import Button from '@material-ui/core/Button';
-// import ActionButton from '../ActionButtonForProvider';
 import PropTypes from 'prop-types';
 import Logbook from '../Logbook/Logbook';
 import GridContainer from "../../components/Grid/GridContainer.jsx";
